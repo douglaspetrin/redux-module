1b58aeb36cbef651c744c9e73c065c30fb80d43f
Douglas Petrin <douglas@douglaspetrin.com>
2019-02-01 21:01:09 +0000

    added redux dev tools! AWESOME!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -16,14 +16,19 @@ const rootReducer = combineReducers({
 
 const logger = store => {
     return next => {
-        return action {
+        return action => {
             console.log('Middleware Disátching: ', action);
+            const result = next(action);
+            console.log('Middleware next state: ', store.getState());
+            return result
         }
 
     }
 };
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
                 <Provider store={store}>
